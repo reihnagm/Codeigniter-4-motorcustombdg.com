a4b160ec1489426dad0f256014889b2610e3d46c
reihan agam <reihanagam7@gmail.com>
2022-06-27 08:22:05 +0700

    update

--- app/Views/layouts/footer.php
+++ app/Views/layouts/footer.php
@@ -8,8 +8,8 @@
                 <div class="footer-col-3">
                     <h3>Follow Us</h3>
                     <ul>
-                        <li>Facebook</li>
-                        <li> <a href="https://wa.me/6289670558381?text=Saya%20tertarik%20dengan%20mobil%20Anda%20yang%20dijual"> WA </a></li>
+                        <li> <a href=""> <img src="<?= base_url() ?>/public/assets/images/instagram.png" width="22"> </a></li>
+                        <li> <a href=""> <img src="<?= base_url() ?>/public/assets/images/wa.png" width="31"> </a></li>
                     </ul>
                 </div>
             </div>
@@ -19,3 +19,5 @@
     </section>
 
 </body>
+
+<!-- https://wa.me/6289670558381?text=Saya%20tertarik%20dengan%20mobil%20Anda%20yang%20dijual -->
